75525c031dc8683e36901caa86b0de1b5649852b
Shaun Inman <shaun@panic.com>
2023-10-14 20:28:24 -0400

    hooked up remote toolchain + added readme

--- support/setup-toolchain.sh
+++ support/setup-toolchain.sh
@@ -2,13 +2,29 @@
 
 # NOTE: I couldn't get relocate.sh to work with this
 # so we just have to recreate the original build path
+TOOLCHAIN_PLATFORM=rgb30
+TOOLCHAIN_VERSION=v001
 TOOLCHAIN_PATH=MOSS/build.MOSS-RK3566.aarch64
-# TODO: download this from github, will probably need to be renamed
-TOOLCHAIN_ZIP=toolchain.tar.gz
-mkdir -p $TOOLCHAIN_PATH
-mv $TOOLCHAIN_ZIP $TOOLCHAIN_PATH
-cd $TOOLCHAIN_PATH
-tar xf $TOOLCHAIN_ZIP
-rm $TOOLCHAIN_ZIP
-cd -
-rm setup-toolchain.sh
+
+TOOLCHAIN_ARCH=`uname -m`
+if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
+	TOOLCHAIN_TAR="$TOOLCHAIN_PLATFORM-toolchain-aarch64.tar.xz"
+else
+	TOOLCHAIN_TAR="$TOOLCHAIN_PLATFORM-toolchain.tar.xz"
+fi
+TOOLCHAIN_URL="https://github.com/shauninman/union-$TOOLCHAIN_PLATFORM-toolchain/releases/download/$TOOLCHAIN_VERSION/$TOOLCHAIN_TAR"
+
+mkdir -p "$TOOLCHAIN_PATH"
+
+if [ -f "./$TOOLCHAIN_TAR" ]; then
+	cp "./$TOOLCHAIN_TAR" "./$TOOLCHAIN_PATH"
+	cd "$TOOLCHAIN_PATH"
+	echo "extracting local toolchain ($TOOLCHAIN_ARCH)"
+else
+	cd "$TOOLCHAIN_PATH"
+	echo "getting remote toolchain ($TOOLCHAIN_URL)"
+	wget "$TOOLCHAIN_URL"
+	echo "extracting remote toolchain $TOOLCHAIN_VERSION ($TOOLCHAIN_ARCH)"
+fi
+tar xf "./$TOOLCHAIN_TAR"
+rm -rf "./$TOOLCHAIN_TAR"
